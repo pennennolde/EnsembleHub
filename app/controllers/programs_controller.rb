d02afc6feae6b98cca999ed97581f84081f08891
pennennolde <m.endoh.0110@gmail.com>
2017-04-06 16:56:27 +0900

    made entry page routing

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -9,5 +9,15 @@ class ProgramsController < ApplicationController
 		@event = Event.find(params[:id])
 	end
 
+	def create
+		Program.create(group: program_params[:group], composer: program_params[:composer], arranger: program_params[:arranger], music: program_params[:music], attention: program_params[:attention], comment: program_params[:comment], event_id: program_params[:id])
+		@event_id = program_params[:id]
+	end
+
+
+	private
+	def program_params
+   		params.permit(:group, :composer, :arranger, :music, :attention, :comment, :id)
+  	end
 
 end
